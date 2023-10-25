a7ca0985a1e78f29f23cc23807dc7b2e2ed4bf80
Ajay Sahani <ajay.sahani@ehubexperts.com>
2023-10-25 12:18:59 +0530

    work on model

--- airbnb-nextjs13/app/components/models/Model.tsx
+++ airbnb-nextjs13/app/components/models/Model.tsx
@@ -60,82 +60,121 @@ const Model: React.FC<ModelProps> = ({
 
     return (
         <>
-            <div className="
-        justfiy-center
-        items-center
-        flex
-        overflow-x-hidden
-        overflow-y-auto
-        fixed
-        inset-0
-        z-50
-        outline-none
-        focus: outline-none
-        bg-neutral-800/70
-        ">
+            <div
+                className="
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
+          focus:outline-none
+          bg-neutral-800/70
+        "
+            >
                 <div className="
-            relative
-            w-full
-            md:w-4/6
-            lg:w-3/6
-            xl:w-2/5
-            mx-auto
-            h-full
-            lg:h-auto
-            md:h-auto
-            ">
-                    {/* Content */}
+          relative 
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/5
+          my-6
+          mx-auto 
+          h-full 
+          lg:h-auto
+          md:h-auto
+          "
+                >
+                    {/*content*/}
                     <div className={`
-                    translate
-                    duration-300
-                    h-full
-                    ${showModel ? 'translate-y-0' : 'translate-y-full'}
-                    ${showModel ? 'opacity' : 'opacity-0'}
-                    `}>
+            translate
+            duration-300
+            h-full
+            ${showModel ? 'translate-y-0' : 'translate-y-full'}
+            ${showModel ? 'opacity-100' : 'opacity-0'}
+          `}>
                         <div className="
-                        translate
-                        h-full
-                        lg:h-auto
-                        md:h-auto
-                        border-0
-                        rounded-lg
-                        shadow-lg
-                        relative
-                        flex
-                        flex-col
-                        w-full
-                        bg-white
-                        outline-none
-                        focus:outline-none
-                        ">
-                            {/* Header */}
+              translate
+              h-full
+              lg:h-auto
+              md:h-auto
+              border-0 
+              rounded-lg 
+              shadow-lg 
+              relative 
+              flex 
+              flex-col 
+              w-full 
+              bg-white 
+              outline-none 
+              focus:outline-none
+            "
+                        >
+                            {/*header*/}
                             <div className="
-                            flex
-                            items
-                            p-6
-                            rounded-t
-                            justify-center
-                            relative
-                            border-b-[1px]
-                            ">
-                                <button className="
-                                p-1
-                                border-0
-                                hover: opacity-70
-                                transition
-                                absolute
-                                left-9
-                                ">
+                flex 
+                items-center 
+                p-6
+                rounded-t
+                justify-center
+                relative
+                border-b-[1px]
+                "
+                            >
+                                <button
+                                    className="
+                    p-1
+                    border-0 
+                    hover:opacity-70
+                    transition
+                    absolute
+                    left-9
+                  "
+                                    onClick={handleClose}
+                                >
                                     <IoMdClose size={18} />
-
                                 </button>
+                                <div className="text-lg font-semibold">
+                                    {title}
+                                </div>
+                            </div>
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
+                                {body}
+                            </div>
+                            {/*footer*/}
+                            <div className="flex flex-col gap-2 p-6">
+                                <div
+                                    className="
+                    flex 
+                    flex-row 
+                    items-center 
+                    gap-4 
+                    w-full
+                  "
+                                >
+                                    {/* {secondaryAction && secondaryActionLabel && (
+                                        <Button
+                                            disabled={disabled}
+                                            label={secondaryActionLabel}
+                                            onClick={handleSecondaryAction}
+                                            outline
+                                        />
+                                    )}
+                                    <Button
+                                        disabled={disabled}
+                                        label={actionLabel}
+                                        onClick={handleSubmit}
+                                    /> */}
+                                </div>
+                                {footer}
                             </div>
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
         </>
     )
