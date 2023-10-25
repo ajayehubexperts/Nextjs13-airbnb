1de981df0045924187472e1339785b3799612ab3
Ajay Sahani <ajay.sahani@ehubexperts.com>
2023-10-25 11:58:38 +0530

    currently work on model and in process Close icon

--- airbnb-nextjs13/app/components/models/Model.tsx
+++ airbnb-nextjs13/app/components/models/Model.tsx
@@ -1,7 +1,143 @@
+'use client';
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from 'react-icons/io';
+interface ModelProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+}
+const Model: React.FC<ModelProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryAction,
+    secondaryLabel
+}) => {
 
-const Model = () => {
+    const [showModel, setShowModel] = useState(isOpen);
+    useEffect(() => {
+        setShowModel(isOpen);
+    }, [isOpen]);
+
+    const handleClose = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+        setShowModel(isOpen);
+        setTimeout(() => {
+            onClose();
+        }, 300)
+    }, [disabled, onClose]);
+
+    const handleSubmit = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+        onSubmit();
+    }, [disabled, onSubmit]);
+
+    const handleSecondaryAction = useCallback(() => {
+        if (disabled || !secondaryAction) {
+            return;
+        }
+    }, [disabled, secondaryAction]);
+
+    if (!open) {
+        return null;
+    }
+
     return (
-        <div>Model</div>
+        <>
+            <div className="
+        justfiy-center
+        items-center
+        flex
+        overflow-x-hidden
+        overflow-y-auto
+        fixed
+        inset-0
+        z-50
+        outline-none
+        focus: outline-none
+        bg-neutral-800/70
+        ">
+                <div className="
+            relative
+            w-full
+            md:w-4/6
+            lg:w-3/6
+            xl:w-2/5
+            mx-auto
+            h-full
+            lg:h-auto
+            md:h-auto
+            ">
+                    {/* Content */}
+                    <div className={`
+                    translate
+                    duration-300
+                    h-full
+                    ${showModel ? 'translate-y-0' : 'translate-y-full'}
+                    ${showModel ? 'opacity' : 'opacity-0'}
+                    `}>
+                        <div className="
+                        translate
+                        h-full
+                        lg:h-auto
+                        md:h-auto
+                        border-0
+                        rounded-lg
+                        shadow-lg
+                        relative
+                        flex
+                        flex-col
+                        w-full
+                        bg-white
+                        outline-none
+                        focus:outline-none
+                        ">
+                            {/* Header */}
+                            <div className="
+                            flex
+                            items
+                            p-6
+                            rounded-t
+                            justify-center
+                            relative
+                            border-b-[1px]
+                            ">
+                                <button className="
+                                p-1
+                                border-0
+                                hover: opacity-70
+                                transition
+                                absolute
+                                left-9
+                                ">
+                                    <IoMdClose size={18} />
+
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
     )
 }
 
